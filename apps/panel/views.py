ccf12592200f511d9ba0078a5e60bf01c3a98514
Juliano Ostroski <juliano.ostroski@gmail.com>
2025-11-06 09:40:40 -0400

    fix: change permission on dashboard settings endpoint

--- apps/panel/views.py
+++ apps/panel/views.py
@@ -1,8 +1,7 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 from .models import DashboardSettings
-from django.conf import settings
 from django.shortcuts import render
 from django.http import JsonResponse
 from apps.dbcom.glpi_queries import get_panel_data, tickets_resolved_today, tickets_open_today
@@ -55,11 +54,12 @@ def api_get_panel_data(request):
         return JsonResponse(error_response, status=500)
 
 
-@api_view(['GET'])
-@permission_classes([IsAuthenticated]) # Protege o endpoint
+@api_view(['GET']) # <-- ESTA LINHA É ESSENCIAL
+@permission_classes([AllowAny])
 def get_dashboard_settings(request):
     """
     Retorna o objeto de configurações do dashboard.
+    Esta view é pública.
     """
     settings_obj = DashboardSettings.objects.get_settings()
     
@@ -67,5 +67,5 @@ def get_dashboard_settings(request):
         'fetch_interval_seconds': settings_obj.fetch_interval_seconds,
         'notification_sound_url': settings_obj.notification_sound_url
     }
+    
     return Response(data)
-
