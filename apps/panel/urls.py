156a89649a50361d04f47f258c9346382f6ce163
Coca-Mann <juliano.ostroski@gmail.com>
2025-11-20 17:09:06 -0400

    fix: remove old dashboard route

--- apps/panel/urls.py
+++ apps/panel/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from apps.panel import views
 
 urlpatterns = [
-    # Rota para a página principal (HTML)
-    path('painel/', views.dashboard_page, name='painel_dashboard'),
-    path('new-panel/', views.new_dashboard_page, name='new_painel_dashboard'),
-    
     # Rota para a API de dados (JSON)
     path('api/dados-painel/', views.api_get_panel_data, name='api_painel_data'),
     path('api/dashboard/settings/', views.get_dashboard_settings, name='api_get_dashboard_settings'),
